64fd2d782deeb3fe65d80d29e1f95f1db2ea6d40
alex <alex.smartens@gmail.com>
2020-10-29 18:06:19 -0400

    Add has_secure_password description

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, 
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
+    # Add methods to set and authenticate against a BCrypt password. This 
+    # mechanism requires you to have a XXX_digest attribute. Where XXX is the 
+    # attribute name of your desired password. The following validations are 
+    # added automatically:
+    # - Password must be present on creation
+    # - Password length should be less than or equal to 72 bytes
+    # - (optional) Confirmation of password (using a XXX_confirmation attribute)
     has_secure_password
 end
