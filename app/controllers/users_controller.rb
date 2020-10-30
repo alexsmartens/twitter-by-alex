d3d0ae327ffe3daf1ebb14263f4ac9544d58e277
alex <alex.smartens@gmail.com>
2020-10-30 16:40:12 -0400

    Permit selected private params in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  # Action, that handles get requests to /users/id
   def show
     @user = User.find(params[:id])
   end
@@ -7,4 +8,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  # Action, that handles post requests /users (by default)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save
+    else 
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
