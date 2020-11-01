f214a7479ac1563a7db06693b11bb9e853ce3f7c
alex <alex.smartens@gmail.com>
2020-11-01 10:15:24 -0500

    Add redirect for successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save
+      redirect_to @user  # the same as redirect_to user_url(@user)
     else 
       render 'new'
     end
