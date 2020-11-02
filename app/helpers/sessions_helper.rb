0ad4ae2200843e8554b20e1c7c4388828de88f08
alex <alex.smartens@gmail.com>
2020-11-02 10:45:37 -0500

    Keep track of current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,20 @@
 module SessionsHelper
 
-  # Log in the given user
+  # Logs in the given user
   def log_in(user)
     # This places a TEMPORARY cookie on the user’s browser containing an ENCRYPTED 
     # version of the user’s id, , which allows us to retrieve the id on subsequent 
     # pages using session[:user_id]. A temprary cookie expires immediately 
     # when the browser is closed.
     session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any)
+  def current_user
+    if session[:user_id]
+      # Equivalent to 
+      # @current_user =  @current_user || User.find_by(id: session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
