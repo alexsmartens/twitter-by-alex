072dec1fb13ce2c22ebd178460a0f22a15b6efc9
alex <alex.smartens@gmail.com>
2020-11-04 12:51:25 -0500

    Add logging in on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save
+      log_in @user
       flash[:success] = "Welcome to the Twitter Sample App!"
       redirect_to @user  # the same as redirect_to user_url(@user)
     else 
