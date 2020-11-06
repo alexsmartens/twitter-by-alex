2cb1bddfc88b3fd211b06ba9ecfc51b576f269d8
alex <alex.smartens@gmail.com>
2020-11-06 12:26:38 -0500

    Add methods required for working with user tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token  # add object attribute
   before_save { self.email = email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX =  /\A[\w\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -16,10 +17,22 @@ class User < ApplicationRecord
   has_secure_password validations: false
   validates :password, presence: true, confirmation: true, length: {minimum: 6}
 
-  # Returns the hash digest of the given string
-  def User.digest(string)
+  # [Class method] Returns the hash digest of the given string
+  def User.digest(string)  # alternative: "self.digest(string)"
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # [Class method] Returns a random token 
+  def User.new_token  # alternative: "self.new_token"
+    SecureRandom.urlsafe_base64
+  end
+
+  # [Instance method] Remembers a user in the database for use in persistant session
+  def remember
+    self.remember_token = User.new_token  # cannot use self.new_token here coz 
+      # 'self' would refer to the instance instead of referring to the class
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
