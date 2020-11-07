d9f1c9a7a4c54fe2696a39ad083ac0afa430e06a
alex <alex.smartens@gmail.com>
2020-11-07 17:30:12 -0500

    Test that the remember_token is properly stored

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
     # authenticate is provided by has_secure_password
     # 'obj&.method' is the same as 'obj && obj.method'
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user  # same as 'redirect_to user_url(userlex)'
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user  # same as 'redirect_to user_url(userlex)'
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
