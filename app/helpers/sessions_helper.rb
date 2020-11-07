bbfdaf5aae46863fa87c3bfa2e4ad130fcff1a5b
alex <alex.smartens@gmail.com>
2020-11-07 15:25:50 -0500

    Implement user forget session actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
 
     # This places a TEMPORARY cookie on the user’s browser containing an ENCRYPTED
     # version of the user’s id, , which allows us to retrieve the id on subsequent
-    # pages using session[:user_id]. A temprary cookie expires immediately
+    # pages using session[:user_id]. A temporary cookie expires immediately
     # when the browser is closed.
     session[:user_id] = user.id
   end
@@ -39,10 +39,19 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise
   def logged_in?
     !!get_current_user
+    @current_user
+  end
+
+  # Forget a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   # Logs out the current user
   def log_out
+    forget(@current_user)
     session.delete(:user_id)
     @current_user = nil
   end
