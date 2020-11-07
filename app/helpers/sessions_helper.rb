b41bf1f51d3172cb808003f6259939ed8192b5c5
alex <alex.smartens@gmail.com>
2020-11-07 11:32:23 -0500

    Remove trailing white spaces

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,13 +2,13 @@ module SessionsHelper
 
   # Logs in the given user
   def log_in(user)
-    # The session object already exists when the page is loaded, so we just need 
-    # to append it with whatever we want to store persistently between user 
-    # requests (browsing the website) 
-    
-    # This places a TEMPORARY cookie on the user’s browser containing an ENCRYPTED 
-    # version of the user’s id, , which allows us to retrieve the id on subsequent 
-    # pages using session[:user_id]. A temprary cookie expires immediately 
+    # The session object already exists when the page is loaded, so we just need
+    # to append it with whatever we want to store persistently between user
+    # requests (browsing the website)
+
+    # This places a TEMPORARY cookie on the user’s browser containing an ENCRYPTED
+    # version of the user’s id, , which allows us to retrieve the id on subsequent
+    # pages using session[:user_id]. A temprary cookie expires immediately
     # when the browser is closed.
     session[:user_id] = user.id
   end
@@ -16,7 +16,7 @@ module SessionsHelper
   # Returns the current logged-in user (if any)
   def current_user
     if session[:user_id]
-      # Equivalent to 
+      # Equivalent to
       # @current_user =  @current_user || User.find_by(id: session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
