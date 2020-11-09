99a9c7785d41994cf87a85ba90ac4b40a4aa0b6b
alex <alex.smartens@gmail.com>
2020-11-09 10:52:14 -0500

    Add sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a main sample users
+User.create!(
+  name: "Example User",
+  email: "example@railstutorial.org",
+  password: "123456",
+  password_confirmation: "123456",
+)
+User.create!(
+  name: "Alex M",
+  email: "alex@example.com",
+  password: "123456",
+  password_confirmation: "123456",
+)
+
+# Generate a bunch of additional users
+98.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "123456"
+
+  # 'create!' method is just like the 'create' method, except it raises an
+  # exception if user validation fails rather than returning false
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+   )
+end
