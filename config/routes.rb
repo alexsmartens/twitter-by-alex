52717b44a8c569cd52e0802b8f09395d3756755f
alex <alex.smartens@gmail.com>
2020-11-09 21:18:50 -0500

    Add account activation resource

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   # Generate a full suite of RESTful routes automatically
   resources :users
+  resources :account_activation, only: [:edit]
 end
