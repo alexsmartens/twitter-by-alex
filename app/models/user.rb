ec82948a119e89fdbb64c446f9567ad0a4ef8bde
alex <alex.smartens@gmail.com>
2020-11-09 22:16:07 -0500

    Add account activation to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token  # add object attribute
-  before_save { self.email = email.downcase}
+  attr_accessor :remember_token, :activation_token
+  # before_save: called every time an object is saved. So for NEW and EXISTING
+  # objects. (create and update action)
+  before_save :downcase_email
+  # before_create: called only before creation. So only for NEW objects (create action)
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX =  /\A[\w\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -48,4 +52,18 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      puts "---------|-create_activation_digest-|---------"
+
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
