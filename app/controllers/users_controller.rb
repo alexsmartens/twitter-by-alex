b7c0ed5d2f2ed248eeb2ebb0a00fbf82b77b2b49
alex <alex.smartens@gmail.com>
2020-11-14 18:00:50 -0500

    Update Users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save
-      log_in @user
-      flash[:success] = "Welcome to the Twitter Sample App!"
-      redirect_to @user  # the same as redirect_to user_url(@user)
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
