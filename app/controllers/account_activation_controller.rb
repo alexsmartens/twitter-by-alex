9d37ddca319f9efec05024b0305122d82bd4935b
alex <alex.smartens@gmail.com>
2020-11-14 19:32:31 -0500

    Add account activation

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -1,2 +1,13 @@
 class AccountActivationController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.assign_attributes({activated: true, activated_at: Time.zone.now})
+      user.save(validate: false)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,17 @@ class SessionsController < ApplicationController
     # 1. Authenticate is provided by has_secure_password
     # 2. 'obj&.method' is the same as 'obj && obj.method'
     if @user&.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # redirect_to @user  # same as 'redirect_to user_url(userlex)'
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        # redirect_to @user  # same as 'redirect_to user_url(userlex)'
+        redirect_back_or @user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link"
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
