28712b9e11effcc6752ebd05adae9fc842e9fded
alex <alex.smartens@gmail.com>
2020-11-15 15:44:45 -0500

    Move account activation actions to User model

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -3,8 +3,7 @@ class AccountActivationController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.assign_attributes({activated: true, activated_at: Time.zone.now})
-      user.save(validate: false)
+      user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
