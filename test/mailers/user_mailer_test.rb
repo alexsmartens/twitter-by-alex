48551433682aec21f28fcc75cdb840139525ce8d
alex <alex.smartens@gmail.com>
2020-11-27 15:44:43 -0500

    Add password_reset test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -17,4 +17,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(@user.email), mail.body.encoded
   end
 
+  test "password_reset" do
+    @user.reset_token = User.new_token
+    mail = UserMailer.password_reset(@user)
+
+    assert_equal "[noreply] Password recovery: Twitter by Alex", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["twitter.by.alex@gmail.com"], mail.from
+    assert_match @user.name, mail.body.encoded
+    assert_match @user.reset_token, mail.body.encoded
+    assert_match CGI.escape(@user.email), mail.body.encoded
+  end
 end
