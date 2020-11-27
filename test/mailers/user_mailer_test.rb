a71984836f21e3e301b7455dfe0da26e889ecf0c
alex <alex.smartens@gmail.com>
2020-11-26 19:11:05 -0500

    Align the test according to the changes in the app

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,9 +6,9 @@ class UserMailerTest < ActionMailer::TestCase
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
 
-    assert_equal "Account activation", mail.subject
+    assert_equal "[noreply] Account activation: Twitter by Alex", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@twitter-by-alex.herokuapp.com"], mail.from
+    assert_equal ["twitter.by.alex@gmail.com"], mail.from
     assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
