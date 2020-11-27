c1d9d9314dcbd1628244d133a80e2bfc43553906
alex <alex.smartens@gmail.com>
2020-11-26 19:22:07 -0500

    Delete duplicated test

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -71,21 +71,4 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
   end
 
-  test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: {
-        user: {
-          name: "Example User",
-          email: "user@example.com",
-          password: "password",
-          password_confirm: "password",
-        }
-      }
-    end
-    follow_redirect!
-    # assert_template 'users/show'
-    # assert flash[:success]
-    # assert is_logged_in?
-  end
 end
