600008568e721d636020a074ff7c4c09d4341f92
alex <alex.smartens@gmail.com>
2020-11-27 16:43:44 -0500

    Code refactoring

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,19 +1,29 @@
 class AccountActivationsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
 
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
-      flash[:success] = "Account activated!"
-      redirect_to user
-    else
-      if user && user.activated?
-        flash[:warning] = "Your account is already activated!"
-      else
-        flash[:warning] = "Incorrect activation token!"
-      end
-      redirect_to root_url
-    end
+    @user.activate
+    log_in @user
+    flash[:success] = "Account activated!"
+    redirect_to @user
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user
+    def valid_user
+      unless @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+        if @user && @user.activated?
+          flash[:warning] = "Your account is already activated!"
+        else
+          flash[:warning] = "Incorrect activation token!"
+        end
+        redirect_to root_url
+      end
+    end
+
 end
