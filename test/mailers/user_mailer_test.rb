1e0b18679a006b3da98a437cd0de21b8c2eaf259
alex <alex.smartens@gmail.com>
2020-11-27 15:44:32 -0500

    Code refactoring

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,16 +1,20 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  def setup
+    @user = users(:michael)
+  end
+
   test "account_activation" do
-    user = users(:michael)
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
+    @user.activation_token = User.new_token
+    mail = UserMailer.account_activation(@user)
 
     assert_equal "[noreply] Account activation: Twitter by Alex", mail.subject
-    assert_equal [user.email], mail.to
+    assert_equal [@user.email], mail.to
     assert_equal ["twitter.by.alex@gmail.com"], mail.from
-    assert_match user.name, mail.body.encoded
-    assert_match user.activation_token, mail.body.encoded
-    assert_match CGI.escape(user.email), mail.body.encoded
+    assert_match @user.name, mail.body.encoded
+    assert_match @user.activation_token, mail.body.encoded
+    assert_match CGI.escape(@user.email), mail.body.encoded
   end
+
 end
