81e08966a5b6d8df0a7bf91ab7722076de2fe470
alex <alex.smartens@gmail.com>
2020-11-27 16:48:06 -0500

    Add password reset edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -14,4 +17,17 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user
+    def valid_user
+      unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+        flash[:warning] = "Incorrect activation token!"
+        redirect_to root_url
+      end
+    end
 end
