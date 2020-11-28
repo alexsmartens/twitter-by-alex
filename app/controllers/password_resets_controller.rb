f7f5c57a6a286ca110ae9b2e151da261bf8718e5
alex <alex.smartens@gmail.com>
2020-11-28 10:23:10 -0500

    Implement user update action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
 
   def new
   end
@@ -18,7 +20,29 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+
+  end
+
+
   private
+
+    def user_params
+      # 1. Only permitted attributes can be assigned/dealt with in this way, this
+      # avoids assigning the attributes that are not intended to be assigned by
+      # the users.
+      # 2. If a permitted attribute is not present than it is considered to be an
+      # empty string
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
@@ -26,8 +50,16 @@ class PasswordResetsController < ApplicationController
     # Confirms a valid user
     def valid_user
       unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
-        flash[:warning] = "Incorrect activation token!"
+        flash[:danger] = "Incorrect activation token!"
         redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired! Try again"
+        redirect_to new_password_reset_url
       end
     end
 end
