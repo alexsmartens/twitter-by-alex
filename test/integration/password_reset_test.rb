80245ed3781342e92b5ddf31e3257186787c55ba
alex <alex.smartens@gmail.com>
2020-11-28 15:04:03 -0500

    Simplify test for invalidating the password reset link on logging in

--- test/integration/password_reset_test.rb
+++ test/integration/password_reset_test.rb
@@ -78,18 +78,10 @@ class PasswordResetTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
-
-    # (6) Reset link used second time (should be invalid)
-    delete logout_path
-    assert_not is_logged_in?
-    patch password_reset_path(user.reset_token),
-    params: { email: user.email,
-              user: { password: "foobar",
-                      password_confirmation: "foobar" } }
-    assert_not is_logged_in?
-    follow_redirect!
-    assert_not flash.empty?
-    assert_select "div.alert.alert-danger", "Incorrect activation token!"
+    # Make sure the link cannot be used after logging in
+    user.reload
+    assert_nil user.reset_digest
+    assert_nil user.reset_sent_at
   end
 
   test "expired token" do
