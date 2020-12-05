7aba8c6b1c3db9c93f3a38b17d421c4007e2ea53
alex <alex.smartens@gmail.com>
2020-12-05 18:03:38 -0500

    Add an option for uploading and displaying an image in a post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # This application adopts a design of one image per post, for more images
+  # there is an option "has_many_attached", which allows multiple files to
+  # be attached to a single Active Record object
+  has_one_attached :image
   default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
