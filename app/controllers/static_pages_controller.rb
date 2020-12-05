d72029efcafe8ee04373c365fca5317a6e13e4a3
alex <alex.smartens@gmail.com>
2020-12-05 14:03:14 -0500

    Fix a bug when feed was executed on a guest (non-logged in) user actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @micropost = get_current_user.microposts.new if logged_in?
-    page_num = params[:page].to_i > 0 ? params[:page] : 1
-    @feed_items = get_current_user.feed.paginate(page: page_num)
+    if logged_in?
+      @micropost = get_current_user.microposts.new
+      page_num = params[:page].to_i > 0 ? params[:page] : 1
+      @feed_items = get_current_user.feed.paginate(page: page_num)
+    end
   end
 
   def help
