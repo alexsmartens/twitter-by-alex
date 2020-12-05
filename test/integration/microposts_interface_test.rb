9fad5d0bd2d6a5dca8be333dd9a1e48f002ca934
alex <alex.smartens@gmail.com>
2020-12-05 18:05:09 -0500

    Add testing image uploading

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,18 +8,23 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost interface" do
     log_in_as(@user)
     get root_path
-    assert_select 'div.pagination', count: 1
+    assert_select "div.pagination", count: 1
+    assert_select "input[type=file]"
     # Invalid submission
-    assert_no_difference 'Micropost.count' do
+    assert_no_difference "Micropost.count" do
       post microposts_path, params: {micropost: {content: ""}}
     end
     assert_select "div#error_explanation"
     assert_select "a[href=?]", "/?page=2"  # Correct pagination link
     # Valid submission
     content = "This micropost really ties the room together"
+    # fixture_file_upload - is rails method for uploading files in tests
+    image = fixture_file_upload("test/fixtures/bmw2.png", "image/png")
     assert_difference "Micropost.count", 1 do
-      post microposts_path, params: {micropost: {content: content}}
+      post microposts_path, params: {micropost: {content: content, image: image}}
     end
+    micropost  = assigns(:micropost)
+    assert micropost.image.attached?
     # Visit different user (no delete links)
     get user_path(users(:archer))
     assert "a", text: "delete", count: 0
