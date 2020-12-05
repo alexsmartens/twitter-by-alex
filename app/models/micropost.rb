f04e8a636a9bfd96a3c5585bb6c3f5da12f2fd7c
alex <alex.smartens@gmail.com>
2020-12-05 18:49:29 -0500

    Limit image file formats and file size

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,9 @@ class Micropost < ApplicationRecord
   default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
+  # validates an image with active_storage_validations gem
+  validates :image, content_type: {in: %w[image/jpeg image/gif image/png],
+                                   message: "must be a valid image format"},
+                    size: {less_than: 5.megabytes,
+                           message: "should be less than 5MB"}
 end
