53a8c79bd478787b503f5b78eeec9aa6e3285c6a
alex <alex.smartens@gmail.com>
2020-12-05 13:21:19 -0500

    Add micropost feed to the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
 
   def home
     @micropost = get_current_user.microposts.new if logged_in?
+    page_num = params[:page].to_i > 0 ? params[:page] : 1
+    @feed_items = get_current_user.feed.paginate(page: page_num)
   end
 
   def help
