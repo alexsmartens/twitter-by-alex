5c7d97dab86e6c5c3cb12abc9c790c5587bb8ba0
alex <alex.smartens@gmail.com>
2020-12-06 10:49:09 -0500

    Add automatic image resizing with variant

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -12,4 +12,19 @@ class Micropost < ApplicationRecord
                                    message: "must be a valid image format"},
                     size: {less_than: 5.megabytes,
                            message: "should be less than 5MB"}
+
+  # Limits both width and height of the image to the specified size
+  def display_image
+    # Dependencies:
+    #   (1) ImageMagic OS package, it comes preinstalled on Heroku
+    #   (2) image_processing gem
+    #   (3) mini_magic gem
+    #
+    # Note 'variant' action:
+    # variant  resizing happens on demand when the method is first called and
+    # is cached for subsequent uses afterwards. For larger sites, it's better to
+    # deffer such processing to a background process.
+    #
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
