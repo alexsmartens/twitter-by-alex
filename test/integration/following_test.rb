71395e1dcc9c883ab99838a7c99d57186d40fe30
alex <alex.smartens@gmail.com>
2020-12-12 18:54:03 -0500

    Add follow/unfollow tests

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -28,4 +29,39 @@ class FollowingTest < ActionDispatch::IntegrationTest
       assert_select ".user_avatars a[href=?]", user_path(user)
     end
   end
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      # 'xhr' attribute stands for XmlHttpRequest; setting xhr to true issues
+      # an Ajax request, which causes the respond_to block in the controller to
+      # execute the proper JavaScript method
+      post relationships_path, params: { followed_id: @other.id }, xhr: true
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    # 'xhr' attribute stands for XmlHttpRequest; setting xhr to true issues
+    # an Ajax request, which causes the respond_to block in the controller to
+    # execute the proper JavaScript method
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
+    end
+  end
+
 end
