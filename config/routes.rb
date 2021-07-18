2fe9a09cf46ce844563eebc18a124698b2e1b193
alex <alex.smartens@gmail.com>
2021-07-18 16:31:09 -0400

    Improve routes structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get '/help', :to => 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+
+  scope controller: :static_pages do
+    get '/help' => :help
+    get '/about' => :about
+    get '/contact' => :contact
+  end
+
   get '/signup', :to => 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+
+  scope controller: :sessions do
+    get '/login' => :new
+    post '/login' => :create
+    delete '/logout' => :destroy
+  end
+
   # "resources :resource" - automatically generate a full suite of RESTful routes
   resources :users do
     # "member" method adds additional routes to respond to URLs containing
@@ -21,6 +29,7 @@ Rails.application.routes.draw do
     #       get :tigers
     #     end
   end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
