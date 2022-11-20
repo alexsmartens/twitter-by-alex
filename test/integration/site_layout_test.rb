d6a61a5794a2e58f7e49a939de51fb39da63cf99
alex <alex.smartens@gmail.com>
2022-11-20 14:18:50 -0800

    Render proper templates and assert proper templates are rendered

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "root layout links" do
     get root_path
-    assert_template 'static_pages/home'
+    assert_template 'static_pages/home_not_logged_in'
     # Assert that there are two links <a href="/">...</a> on the page
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
