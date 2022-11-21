f295e01c9484a8f1a09b95d17791824a796fc200
alex <alex.smartens@gmail.com>
2022-11-21 10:21:17 -0800

    Cleaning up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    # Serves login view
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    # 1. Authenticate is provided by has_secure_password
-    # 2. 'obj&.method' is the same as 'obj && obj.method'
     if @user&.authenticate(params[:session][:password])
       if @user.activated?
         log_in @user
         remember(@user) if params[:session][:remember_me] == '1'
         redirect_back_or root_url
       else
-        message = "Account not activated. "
-        message += "Check your email for the activation link"
+        message = "Account not activated. Check your email for the activation link"
         flash[:warning] = message
         redirect_to root_url
       end
@@ -25,6 +23,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to  root_url
+    redirect_to root_url
   end
 end
