75b61d3a81fbb89d8a9007082589b508d76df14c
alex <alex.smartens@gmail.com>
2022-11-21 12:12:27 -0800

    Add custom error handling for 404 and 500 (more info: https://web-crunch.com/posts/custom-error-page-ruby-on-rails)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
+  # Shows full error reports if true. Set to false to test custom exception
+  # handling pages.
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
